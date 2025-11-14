7a2016499abc68153d080d5259055a67878dc7c0
Guilherme Ferraresi <gui.ferraresi2008@gmail.com>
2025-11-14 08:00:12 -0300

    Adição da Classe de Login

---
+++ Login/Libraries/Login/LoginCliente.cs
@@ -0,0 +1,21 @@
+﻿using Login.Libraries.Sessao;
+using Login.Models;
+using Newtonsoft.Json;
+using System.Security.Cryptography.X509Certificates;
+namespace Login.Libraries.Login
+{
+    public class LoginCliente
+    {
+        private string Key = "Login.Cliente";
+        private Sessao.Sessao _sessao;
+        public LoginCliente(Sessao.Sessao sessao)
+        {
+            _sessao = sessao;
+        }
+        public void Login(Cliente cliente)
+        {
+            string clienteJSONstring = JsonConvert.SerializeObject(cliente);
+            _sessao.Cadastrar(Key, clienteJSONstring);
+        }
+    }
+}
